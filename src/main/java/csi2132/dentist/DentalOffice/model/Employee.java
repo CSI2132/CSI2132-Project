3ee15d67854cc981c95b1aa67a090f83f503e92b
idzidz <idz2612@gmail.com>
2022-04-01 22:09:22 -0400

    Employee model + Employee POST

---
+++ src/main/java/csi2132/dentist/DentalOffice/model/Employee.java
@@ -0,0 +1,15 @@
+package csi2132.dentist.DentalOffice.model;
+
+public class Employee {
+    private Integer user_id;
+    private String employee_role;
+
+
+    public Integer getUser_Id() { return user_id; }
+    public void setUser_Id(Integer user_id){ this.user_id = user_id; }
+
+    public String getEmployee_role() { return employee_role; }
+    public void setEmployee_role(String employee_role){ this.employee_role = employee_role; }
+}
+
+
--- src/main/java/csi2132/dentist/DentalOffice/service/EmployeeService.java
+++ src/main/java/csi2132/dentist/DentalOffice/service/EmployeeService.java
@@ -1,15 +1,17 @@
-// package csi2132.dentist.DentalOffice.service;
+package csi2132.dentist.DentalOffice.service;
 
-// // import csi2132.dentist.DentalOffice.model.Employee;
-// import csi2132.dentist.DentalOffice.repository.EmployeeRepository;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import csi2132.dentist.DentalOffice.model.Employee;
+import csi2132.dentist.DentalOffice.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// @Service
-// public class EmployeeService {
+@Service
+public class EmployeeService {
 
-//     @Autowired
-//     private EmployeeRepository employeeRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
-
-// }
+    public Integer createEmployee(Employee employee){
+        return employeeRepository.createEmployee(employee);
+    }
+}
