9772c0b91ec3a5f0ff4415c1a5dbb829e2483f3c
Michael Kagnew <mike.kagnew@gmail.com>
2022-04-13 21:51:05 -0400

    created patient page

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Login from './Login';
 import Dentist from './Dentist';
 import Receptionist from './Receptionist';
+import Patient from './Patient';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Route exact path="/" element={<Login/>} ></Route>
         <Route exact path="/dentist" element={<Dentist/>}></Route>
         <Route exact path="/receptionist" element={<Receptionist />} ></Route>
-
+        <Route exact path="/patient" element={<Patient />} ></Route>
       </Routes>
     </Router>
   );
--- client/client/src/Patient.js
+++ client/client/src/Patient.js
@@ -2,22 +2,21 @@ import { useEffect, useState } from "react";
 
 function Patient(props) {
   const [medicalHistory, setMedicalHistory] = useState([]);
+  const [upcomingAppointments, setUpcomingAppointments] = useState([]);
+  const userId = 47;
   useEffect(() => {
-    fetch(`http://localhost:8080/appointment/getPatientRecord/${props.userId}`).then(async (result) => {
+    fetch(`http://localhost:8080/appointment/getPatientRecord/${userId}`).then(async (result) => {
       if (result.ok) {
         setMedicalHistory(await result.json());
       }
     });
-  });
 
-  const [upcomingAppointments, setUpcomingAppointments] = useState([]);
-  useEffect(() => {
-    fetch(`http://localhost:8080/appointment/getAppointmentByPatientId${props.userId}`).then(async (result) => {
+    fetch(`http://localhost:8080/appointment/getAppointmentByPatientId/${userId}`).then(async (result) => {
       if (result.ok) {
         setUpcomingAppointments(await result.json());
       }
     });
-  });
+  }, []);
 
   return (
     <div>
@@ -41,9 +40,6 @@ function Patient(props) {
 }
 
 function getMedicalHistory(records) {
-  const labels = [
-
-  ];
 
   const keys = [
     "appointment_type",
@@ -78,14 +74,16 @@ function getMedicalHistory(records) {
   // use <summary>
 
   const result = [];
+  console.log(records);
   for (const record of records) {
     result.push(
-      <>
+      <details className="border">
         <summary>
+        
           {`Treatment at ${record.treatment_date}`}
         </summary>
-        <div>
-          {() => {
+        <div className="border">
+          {(() => {
             const result = [];
             for (let i = 0; i < keys.length; i++) {
               result.push(
@@ -95,61 +93,61 @@ function getMedicalHistory(records) {
               )
             }
             return result;
-          }}
+          })()}
         </div>
-      </>
+      </details>
     );
   }
+
+  return result;
 }
 
 function getUpcomingAppointments(appointments) {
+  const keys = [
+    "appointment_type",
+    "assigned_room",
+    "start_time",
+    "end_time",
+    "hygienist_first_name",
+    "hygienist_last_name",
+  ];
+
+  const names = [
+    "Appointment Type",
+    "Assigned Room",
+    "Start Time",
+    "End Time",
+    "Hygienist First Name",
+    "Hygienist Last Name",
+  ];
+
+  // use <summary>
+  
   const result = [];
   for (const appointment of appointments) {
     result.push(
-      <tr>
-        <td>
-          {appointment["aa.appointment_date"]}
-        </td>
-        <td>
-          {appointment["aa.start_time"]}
-        </td>
-        <td>
-          {appointment["aa.end_time"]}
-        </td>
-        <td>
-          {appointment["aa.assigned_room"]}
-        </td>
-        <td>
-          {appointment["aa.appointment_type"]}
-        </td>
-        <td>
-          {appointment["dentist.first_name"] + appointment["dentist.last_name"]}
-        </td>
-        <td>
-          {appointment["hygenist.first_name"] + appointment["hygenist.last_name"]}
-        </td>
-      </tr>
+      <details className="border">
+        <summary>
+          {`Appointment at ${appointment.appointment_date} with ${appointment.dentist_first_name} ${appointment.dentist_last_name}`}
+        </summary>
+        <div className="border">
+          {(() => {
+            const result1 = [];
+            for (let i = 0; i < keys.length; i++) {
+              result1.push(
+                <div>
+                  <b>{names[i]}</b>: {appointment[keys[i]]}
+                </div>
+              )
+            }
+            return result1;
+          })()}
+        </div>
+      </details>
     );
   }
 
-  return (
-    <table>
-      <thead>
-        <tr>
-            <th>Appointment Date</th>
-            <th>Start Time</th>
-            <th>End Time</th>
-            <th>Room</th>
-            <th>Type</th>
-            <th>Dentist</th>
-            <th>Hygenist</th>
-        </tr>
-      </thead>
-      <tbody>
-        {result}
-      </tbody>
-    </table>
-  );
+  return result;
 }
 
 export default Patient;
